4b47cf4d1bb29423020acfb69a85eeb344cb4eca
erliinaaap <erlinap173@gmail.com>
2025-06-17 09:52:17 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -30,6 +30,13 @@
         ?>
 
         <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Nav -->
+
+        <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'produk_kategori') ? "" : "collapsed" ?>" href="produk_kategori">
                 <i class="bi bi-receipt"></i>
                 <span>Kategori Produk</span>
